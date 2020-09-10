46eef2bd381a911e03a112d577c3b1b9ff3a4e9b
Daniel-Rauh <daniel.rauh@gmail.com>
2020-09-10 16:13:38 +0200

    test

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.set('views', './views');
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 let data = require('./data.json')
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
     console.log("I'm listening")
 })
 
